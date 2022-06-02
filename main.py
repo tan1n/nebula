71708213d626c27f30d66ec6a4b76c62597dc477
tan1n <taninrahman07@gmail.com>
2022-06-02 21:34:11 +0600

    updated factor and rotation

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ import serial
 import mss.tools
 
 start_tine = time()
-frame = 0
-
+com_port = '/dev/ttyACM0'
 resolution = (1920, 1080)
 factor = 10
 
 dimensions = [
-    [{"top": 10, "left": 10, "width": 1910, "height": 1}, 'top'],  # top
-    [{"top": 11, "left": 1910, "width": 1, "height": 1070}, 'right'],  # right
-    [{"top": 1070, "left": 10, "width": 1910, "height": 1}, 'bottom'],  # bottom
-    [{"top": 11, "left": 10, "width": 1, "height": 1070}, 'left']  # left
+    [{"top": factor, "left": factor, "width": resolution[0] -
+        factor, "height": 1}, 'top'],  # top
+    [{"top": factor+1, "left": resolution[1]-factor, "width": 1,
+        "height": resolution[1]-factor}, 'right'],  # right
+    [{"top": resolution[1]-factor, "left": factor,
+        "width": resolution[0]-factor, "height": 1}, 'bottom'],  # bottom
+    [{"top": factor+1, "left": 10, "width": 1,
+        "height": resolution[1]-factor}, 'left']  # left
 ]
 
-led_width = 20
-led_height = 13
+led_width = 15
+led_height = 8
 
 
 def capture(args):
@@ -27,26 +30,33 @@ def capture(args):
         1, led_height)
     with mss.mss() as sct:
         screen = np.array(sct.grab(args[0]))
-        img = cv2.resize(screen, res,
-                         interpolation=cv2.INTER_NEAREST)
+        img = cv2.resize(screen, res, interpolation=cv2.INTER_NEAREST)
         output = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return np.array(output).flatten().tolist()
-
+        pixel_data = np.array(output).flatten().tolist()
+        if args[1] == 'bottom' or args[1] == 'left':
+            return pixel_data.reverse()
+        else:
+            return pixel_data
 
 def write(data, link):
     for x, i in enumerate(data):
         char = bytes([i])
         link.write(char)
+        print(char)
 
-
-try:
-    link = serial.Serial(port='COM3', baudrate=115200, timeout=1)
+def main():
+    link = serial.Serial(port=com_port, baudrate=115200, timeout=1)
     sleep(3)
     write(data=[0, 0, 0], link=link)
     while True:
         for dimension in dimensions:
             data = capture(dimension)
             write(data, link)
-except KeyboardInterrupt:
-    link.close()
-    pass
+
+# try:
+#     main();
+# except Exception:
+#     # link.close()
+#     pass
+
+main()
